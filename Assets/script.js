7a39319ca321889f9f8c0960588f52f400138e42
SLeBron <sophieclebron@gmail.com>
2022-10-19 11:45:38 -0600

    added variables to script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,11 @@
 let apiKey = 'a5ceee039d9340faa7953b042ac20168';
 let cityInput = document.querySelector("#search");
+let searchBtn = document.querySelector("#search-btn");
+let tempEl = document.getElementById("temperature");
+let humidityEl = document.getElementById("humidity");
+let windEl = document.getElementById("wind");
+let forecastDiv = document.getElementById("forecast");
+let weatherDashboard = document.getElementById("city");
+let date = new Date().toUTCString().slice(5, 16);
+
+
