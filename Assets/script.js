07e4c7d41bf4cd726ed6650c0c9171d6a57b6879
SLeBron <sophieclebron@gmail.com>
2022-10-20 14:33:13 -0600

    updating

--- Assets/script.js
+++ Assets/script.js
@@ -8,4 +8,79 @@ let forecastDiv = document.getElementById("forecast");
 let weatherDashboard = document.getElementById("city");
 let date = new Date().toUTCString().slice(5, 16);
 
+function weatherFunction(city){
+    let apiParams = new URLSearchParams({
+        key:apiKey,
+        city,
+        units: "I"
+    })
 
+    let currentweatherAPI = 
+    "https://api.weatherbit.io/v2.0/current?"+apiParams;
+        fetch(currentweatherAPI)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            currentWeather(data)
+            console.log(data);
+        });
+
+    let currentWeather = function(current){
+        let weatherIcon = document.createElement("img");
+            weatherIcon.src = "./assets/icons/" + current.data[0].weather.icon + ".png";
+        let city = document.getElementById("city");
+        city.textContent=current.data[0].city + "" + date;
+
+        tempEl.textContent = current.data[0].temp + "°F";
+        humidityEl.textContent = current.data[0].rh + "%";
+        windEl.textContent = current.data[0].wind_spd + "MPH";
+        city.appendChild(weatherIcon);
+    }
+
+    let forecastweatherAPI = 
+    "https://api.weatherbit.io/v2.0/forecast/daily?"+apiParams;
+
+    fetch(forecastweatherAPI)
+    .then(function (response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        forecastWeather(data)
+        console.log(data);
+    });
+
+    let forecastWeather = function(forecast) {
+        for (let i=1; i<6; i++) {
+            let icon = document.getElementById("icon"+[i]);
+            let cardTitle = document.getElementById("date"+[i]);
+            let forecastTemp = document.getElementById("temperature"+[i]);
+            let forecastHumidity = document.getElementById("humidity"+[i]);
+            let forecastWind = document.getElementById("wind"+[i]);
+
+            cardTitle.textContent = "Forecast:" + forecast.data[i].valid_date;
+            forecastTemp.textContent = "Temperature:" + forecast.data[i].temp + "°F";
+            forecastHumidity.textContent = "Humidity:" + forecast.data[i].rh + "%";
+            forecastWind.textContent = "Wind:" + forecast.data[i].wind_spd + "MPH";
+        }
+    }
+}
+
+searchBtn.addEventListener('click', function() {
+    let city = cityInput.value;
+    weatherFunction(city);
+
+    let saveSection = document.getElementById('cities');
+    let liEl = document.createElement("li");
+    let searchSaveBtn = document.createElement("button");
+
+    saveSection.append(liEl);
+    liEl.append(searchSaveBtn);
+
+    searchSaveBtn.textContent = cityInput.value;
+    sarchSaveBtn = addEventListener('click', function() {
+        weatherFunction(city);
+    })
+})
